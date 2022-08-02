1051902c6e0797645ba1c684b3fa40744eb8d7c1
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2022-08-02 12:19:14 +0400

    feat: add image slider

--- momentum/assets/scripts/index.js
+++ momentum/assets/scripts/index.js
@@ -1,5 +1,12 @@
 import * as slider from './slider.js';
 
 const body = document.querySelector('body');
+const main = document.querySelector('main');
+const sliderButtonLeft = main.querySelector('.left');
+const sliderButtonRight = main.querySelector('.right');
 
 body.style.backgroundImage = slider.setBackground();
+
+sliderButtonLeft.addEventListener('click', e => body.style.backgroundImage = slider.changeSlide(body, -1));
+sliderButtonRight.addEventListener('click', e => body.style.backgroundImage = slider.changeSlide(body, 1));
+
--- momentum/assets/scripts/slider.js
+++ momentum/assets/scripts/slider.js
@@ -1,9 +1,17 @@
-export function setBackground() {
+export function setBackground(imageIndex = null) {
   const timeOfDay = 'afternoon';
-  const imageNumber = getRandomNumber(1, 21).toString().padStart(2, '0');
-  const url = `https://raw.githubusercontent.com/VaultDweller13/stage1-tasks/assets/images/${timeOfDay}/${imageNumber}.jpg`
+  const imageNumber = imageIndex ? imageIndex : getRandomNumber(1, 21).toString().padStart(2, '0');
 
-  return `url('${url}')`;
+  return `url(https://raw.githubusercontent.com/VaultDweller13/stage1-tasks/assets/images/${timeOfDay}/${imageNumber}.jpg)`;
+}
+
+export function changeSlide(element, offset) {
+  let urlArr = element.style.backgroundImage.split('/');
+  let slideNumber = +urlArr[urlArr.length - 1].split('.')[0] + offset;
+
+  slideNumber = slideNumber < 1 ? 20 : slideNumber > 20 ? 1 : slideNumber;  
+
+  return setBackground(slideNumber.toString().padStart(2, '0'));
 }
 
 function getRandomNumber(min, max) {
