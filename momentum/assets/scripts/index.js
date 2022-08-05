2886bb10d15dbb9ad72bce35d095dd5897926f7b
VaultDweller13 <grognak13@gmail.com>
2022-08-06 01:21:55 +0400

    feat: add weather widget logic using promises

--- momentum/assets/scripts/index.js
+++ momentum/assets/scripts/index.js
@@ -3,12 +3,27 @@ import * as dateTime from './datetime.js';
 import * as weather from './weather.js'
 
 const body = document.querySelector('body');
-const main = document.querySelector('main');
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const footer = document.querySelector('.foooter');
+
+// Slider constants
 const sliderButtonLeft = main.querySelector('.left');
 const sliderButtonRight = main.querySelector('.right');
+
+// Date, time, greeting blocks constants
 const dateBlock = main.querySelector('.date');
 const timeBlock = main.querySelector('.time');
 const greetingBlock = main.querySelector('.greeting-message');
+
+// Weather block constants
+const weatherBlock = header.querySelector('.weather-container');
+const city = weatherBlock.querySelector('.weather-input');
+const weatherIcon = weatherBlock.querySelector('.weather-icon');
+const temperature = weatherBlock.querySelector('.weather-temperature');
+const clouds = weatherBlock.querySelector('.weather-clouds');
+const wind = weatherBlock.querySelector('.weather-wind');
+const humidity = weatherBlock.querySelector('.weather-humidity');
 
 // Set background
 body.style.backgroundImage = slider.setBackground(dateTime.getTimeOfDay());
@@ -23,4 +38,14 @@ sliderButtonLeft.addEventListener('click', e => body.style.backgroundImage = sli
 sliderButtonRight.addEventListener('click', e => body.style.backgroundImage = slider.changeSlide(body, 1));
 
 // Get weather
-weather.getWeather();
+function getWeather(city) {
+  weather.getWeather(city).then(weather => {
+    temperature.textContent = `${weather.temp} ℃`;
+    clouds.textContent = weather.clouds;
+    wind.textContent = `${weather.wind} m/s`;
+    humidity.textContent = `${weather.humidity}%`;
+  })
+}
+
+getWeather(city.value);
+city.addEventListener('change', () => getWeather(city.value));
--- momentum/assets/scripts/weather.js
+++ momentum/assets/scripts/weather.js
@@ -1,21 +1,23 @@
-const key = 'dd78fca7a4c44e2da151ed13848bf896';
-const cityName = 'Minsk';
+const KEY = 'dd78fca7a4c44e2da151ed13848bf896';
 
-export async function getWeather() {
-  const coords = await getCityCoordinats();
-
-  if (!Array.isArray(coords)) {
-    console.log('error');
-    return 'error';
-  }
-
-  const weather = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=${key}`);
-  weather.then(res => res.json()).then(data => console.log(data));
+export function getWeather(city) {
+  return getCityCoordinates(city)
+    .then(res => loadJson(`https://api.openweathermap.org/data/2.5/weather?lat=${res[0]}&lon=${res[1]}&units=metric&appid=${KEY}`))
+    .then(getWeatherData);
 }
 
-function getCityCoordinats() {
-  const coords = [];
-  const city = fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`);
+const loadJson = url => fetch(url).then(res => res.json());
 
-  return city.then(res => res.json()).then(data => [data[0].lat, data[0].lon]);
+function getCityCoordinates(city) {
+  return loadJson(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${KEY}`)
+    .then(data => [data[0].lat, data[0].lon]);
+}
+
+function getWeatherData(data) {
+  return {
+    temp: data.main.temp,
+    humidity: data.main.humidity,
+    wind: data.wind.speed,
+    clouds: data.weather[0].description
+  }
 }
