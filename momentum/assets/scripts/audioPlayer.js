476cbd13ed97111392c8fbd41a692b3f09364c2e
VaultDweller13 <grognak13@gmail.com>
2022-08-18 01:06:41 +0400

    feat: add advanced audio-player, first version

--- momentum/assets/scripts/audioPlayer.js
+++ momentum/assets/scripts/audioPlayer.js
@@ -1,23 +1,35 @@
 export class AudioPlayer {
-  constructor(playList){
+  constructor(element, playList){
+    const durationBlock = element.querySelector('.track-duration'); 
+
     this.playList = playList.map(item => {
       const description = `${item.title} - ${item.artist}`;
       const audio = document.createElement('audio');
-
-      audio.textContent = 'Your browser don\'t support audio playback';
+     
+      audio.textContent = 'Your browser doesn\'t support audio playback';
       audio.src = item.src;
+      audio.preload = 'metadata';
+      
       return {
         audio: audio,
-        description: description
+        description: description,
+        duration: item.duration
       };
     });
 
+    this.widget = element;
     this.current = this.playList[0].audio;
+    this.volume = 1;
   }
 
   play() {
+    const track = this.widget.querySelector('.track');
+    this.current.volume = this.volume;
     this.current.play();
     this.current.parentElement.classList.add('current-track');
+
+    track.textContent = this.getCurrentTrack().description;
+    this.showDuration();
   }
 
   pause() {
@@ -54,20 +66,61 @@ export class AudioPlayer {
     this.play();
   }
 
+  mute() {
+    if (this.volume) {
+      this.volume = 0;
+    } else this.volume = 1;
+
+    this.current.volume = this.volume
+  }
+
   isPaused() {
     return this.current.paused;
   }
 
-  showPlayList(element) {
+  showPlayList() {
+    const trackList = this.widget.querySelector('.audio-player');
     this.playList.forEach(item => {
       const li = document.createElement('li');
+      const audio = item.audio;
+      
       li.textContent = item.description;
       li.appendChild(item.audio);
-      element.appendChild(li);
+      trackList.appendChild(li);
     });
+
+    const track = this.widget.querySelector('.track');
+    track.textContent = this.getCurrentTrack().description;
+
+    this.showDuration();
+  }
+
+  showDuration() {
+    const durationBlock = this.widget.querySelector('.track-duration'); 
+    const progressLine = this.widget.querySelector('.progress');
+    const duration = formatTime(this.getCurrentTrack().duration);
+    const currentTime = formatTime(this.current.currentTime);
+
+    durationBlock.textContent = `${currentTime}/${duration}`;
+    progressLine.style.width = Math.floor(+this.current.currentTime/+this.getCurrentTrack().duration*100)  + '%';
+
+    setTimeout(() => {
+      this.showDuration();
+    }, 1000);
+
+    function formatTime(time) {
+      const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+      const seconds = Math.floor(time % 60).toString().padStart(2, '0');
+
+      return `${minutes}:${seconds}`;
+    }
   }
 
   #getCurrentIndex() {
     return this.playList.findIndex(item => item.audio === this.current);
+  }
+
+  getCurrentTrack() {
+    return this.playList[this.#getCurrentIndex()];
   }
 } 
--- momentum/assets/scripts/playList.js
+++ momentum/assets/scripts/playList.js
@@ -3,24 +3,24 @@ export const playList = [
     title: 'A love theme',
     artist: 'Eugenio Mininni',
     src: 'assets/files/music/eugenio-mininni-a-love-theme.mp3',
-    duration: '2:04',
+    duration: '124',
   },
   {
     title: 'Driving ambition',
     artist: 'Ahjay Stelino',
     src: 'assets/files/music/ahjay-stelino-driving-ambition.mp3',
-    duration: '1:42',
+    duration: '102',
   },
   {
     title: 'Valley sunset',
     artist: 'Alejandro Magaña',
     src: 'assets/files/music/alejandro-magana-valley-sunset.mp3',
-    duration: '2:13',
+    duration: '133',
   },
   {
     title: 'Complicated',
     artist: 'Arulo',
     src: 'assets/files/music/arulo-complicated.mp3',
-    duration: '1:49',
+    duration: '109',
   },
 ]
